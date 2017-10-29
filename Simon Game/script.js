d26b0b88c19e6a3d8085cbfe6f5e61ff694fa6d5
Ivan <ivan3123708@gmail.com>
2017-10-29 21:24:29 +0100

    Regular update

--- Simon Game/script.js
+++ Simon Game/script.js
@@ -1,7 +1,15 @@
+var sounds = {
+    green: new Audio('audio/sound1.mp3'),
+    red: new Audio('audio/sound2.mp3'),
+    yellow: new Audio('audio/sound3.mp3'),
+    blue: new Audio('audio/sound4.mp3')
+};
+
 $(document).ready(function() {
 
     var simon = {
         isOn: false,
+        strict: false,
         session: [],
         userSession: []
     }
@@ -12,42 +20,74 @@ $(document).ready(function() {
         simon.session.length = 0;
         simon.userSession.length = 0;
 
+        $('#start').css('background', 'rgb(116, 29, 29)');
+        $('#strict').css('background', 'rgb(116, 29, 29)');
+
         simon.isOn ? $('#isOn').css('background', 'rgb(41, 233, 41)') : $('#isOn').css('background', 'rgb(34, 85, 34)');
-        simon.isOn ? $('#screen').html('- -') : $('#screen').html('');
-        
+        simon.isOn ? $('#screen').html('--') : $('#screen').html('');
     });
 
-    $('#start').on('click', function() {
 
-        $('#start').css('background', 'red');
+    $('#start').click(function() {
 
-        randomMove(simon);
+        if(simon.isOn) {
 
+            $('#start').css('background', 'red');
+            $('#screen').html('***');
+
+            simon.session.length = 0;
+            simon.userSession.length = 0;
+
+            randomMove(simon);
+        }
     });
-
+    
     $('#strict').on('click', function() {
 
-        $('#strict').css('background', 'red');
+        if(simon.isOn) {
+
+            $('#strict').css('background', 'red');
+
+            simon.strict = !simon.strict;
+
+        }
     });
 
     $('.colorBtn').on('click', function() {
 
-        light($(this).attr('id'));
+        if(simon.isOn) {
 
-        simon.userSession.push($(this).attr('id'));
-        console.log('user session is: ' + simon.userSession);
-        for(var i = 0; i < simon.userSession.length; i++) {
-    
-            if(simon.userSession[i] === simon.session[i]) {
-                if(simon.userSession.length === simon.session.length) {
-                    randomMove(simon);
-                    if(simon.session.length > 5) {
-                        console.log('WIN');
+            sounds[$(this).attr('id')].play();
+
+            lightOn($(this).attr('id'));
+            
+            simon.userSession.push($(this).attr('id'));
+            console.log('user session is: ' + simon.userSession);
+            for(let j = 0; j < simon.userSession.length; j++) {
+        
+                if(simon.userSession[j] === simon.session[j]) {
+                    if(simon.userSession.length === simon.session.length) {
+                        if(simon.userSession[simon.userSession.length - 1] === simon.session[simon.session.length -  1]) {
+                            if(simon.session.length >= 3) {
+                                console.log('WIN');
+                                $('#screen').html('WIN');
+                                setTimeout(function() {
+                                   start(simon); 
+                                }, 3000);
+                            } else {
+                                randomMove(simon);
+                            }
+                        } else {
+                            $('#screen').html('WRONG');
+                            wrong(simon);
+                        }
                     }
                 }
-            }  
-            else {
-                console.log('WRONG');
+                else {
+                    $('#screen').html('WRONG');
+                    
+                    wrong(simon);
+                }
             }
         }
     });
@@ -63,14 +103,19 @@ function randomMove(obj) {
 
     obj.session.push(colors[random]);
 
-    light(colors[random]);
-
-    obj.session.length <= 5 ? $('#screen').html(obj.session.length) : $('#screen').html('WIN');
+    for(let i = 0; i < obj.session.length; i++) {
+        setTimeout(function() {
+            sounds[obj.session[i]].play();
+            lightOn(obj.session[i]);
+            $('#screen').html(obj.session.length);
+        }, 2000 + i * 1000);
+        
+    }
 
     console.log('session is: ' + obj.session);
 }
 
-function light(color) {
+function lightOn(color) {
 
     switch(color) {
         case 'green':
@@ -86,4 +131,55 @@ function light(color) {
             $('#blue').css('background-color', 'rgb(71, 157, 255)');
             break;
     }
+
+    setTimeout(function() {
+        
+        switch(color) {
+            case 'green':
+                $('#green').css('background-color', 'rgb(0, 212, 0)');
+                break;
+            case 'red':
+                $('#red').css('background-color', 'rgb(212, 0, 0)');
+                break;
+            case 'yellow':
+                $('#yellow').css('background-color', 'rgb(209, 209, 0)');
+                break;
+            case 'blue':
+                $('#blue').css('background-color', 'rgb(0, 96, 206)');
+                break;
+        }
+    }, 900);
+}
+
+function wrong(obj) {
+
+    if(!obj.strict) {
+
+        for(let i = 0; i < obj.session.length; i++) {
+            setTimeout(function() {
+                sounds[obj.session[i]].play();
+                lightOn(obj.session[i]);
+                obj.session.length <= 5 ? $('#screen').html(obj.session.length) : $('#screen').html('WIN');
+            }, 3000 + i * 1000);
+            
+        }
+
+    } else {
+
+        start(obj);
+    }
+}
+
+function start(obj) {
+
+    $('#start').css('background', 'red');
+    $('#screen').html('***');
+
+    obj.session.length = 0;
+    obj.userSession.length = 0;
+
+    setTimeout(function() {
+        randomMove(obj);
+    }, 2000);
+    
 }
